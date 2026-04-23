bd4f215908a313b4abd7551419b7399ff5fa45ca
Nicolas-811 <nicolas.ramirezreales@gmail.com>
2026-04-23 15:35:15 -0400

    len_ejercicios

---
+++ ejercicios.py
@@ -0,0 +1,8 @@
+name1="catalina"
+name2="vicente"
+
+print(name1.strip()) #quitar espacio
+print(name1.lower()) #minuscula
+print(name1.upper()) #mayuscula
+print(name1.replace("pinochet", "frei")) #reemplazar catalina frei
+print(name1.find("pinochet")) #encontrar la posicion de la palabra
---
+++ shazam.py
@@ -0,0 +1,20 @@
+# clave="shazam"
+# palabra=input("ingrese la clave: ").lower()
+# if clave==palabra:
+#     print("la contraseña es correcta")
+# else:
+#     print("la contraseña es incorrecta")
+
+
+# nombre_usuario=input("Ingrese su nombre de usuario: ")
+# print(len(nombre_usuario))
+# if len(nombre_usuario)>=4 and len(nombre_usuario)<=10:
+#     print("su rango esta entre 4 y 10 caracteres")
+# else:
+#     print("su rango esta fuera de los caracteres")
+
+# gif
+
+
+
+
